bd1b3f04723013f6c6d3f26ac682cf5774ad1c5d
Javier <javi2gar@gmail.com>
2017-10-03 21:05:24 +0100

    Modificado los elementos

--- app/src/main/java/com/cip/javier/ut03_act01/MainActivity.java
+++ app/src/main/java/com/cip/javier/ut03_act01/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,17 +37,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+    public boolean onOptionsItemSelected (MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    //noinspection SimplifiableIfStatement
+        String mensaje;
+        switch (id) {
+            case R.id.action_buscar:
+                mensaje = "Elemento buscar seleccionado" ;
+                Toast.makeText( this , mensaje, Toast.LENGTH_LONG).show();
+                return true ;
+            case R.id.action_nuevo:
+                mensaje = "Elemento nuevo seleccionado" ;
+                Toast.makeText( this , mensaje, Toast.LENGTH_LONG).show();
+                return true ;
+            case R.id.action_settings:
+                mensaje = "Elemento configurar seleccionado" ;
+                Toast.makeText( this , mensaje, Toast.LENGTH_LONG).show();
+                return true ;
         }
-
-        return super.onOptionsItemSelected(item);
+        return super .onOptionsItemSelected(item);
     }
 }
+
